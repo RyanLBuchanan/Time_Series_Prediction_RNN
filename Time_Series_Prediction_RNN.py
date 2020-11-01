a35ba798b49e2b0d2770d79b03cd28add57d434a
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-11-01 13:07:48 -0700

    feat: Added next step of code from tutorial -broken, need to see documentation

--- Time_Series_Prediction_RNN.py
+++ Time_Series_Prediction_RNN.py
@@ -27,3 +27,51 @@ plt.plot(time_steps[1:], y, 'b.', label='target, y') # y
 plt.legend(loc='best')
 plt.show()
 
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, output_size, hidden_dim, n_layers):
+        super(RNN, self).__init__()
+        
+        self.hidden_dim=hidden_dim
+
+        # define an RNN with specified parameters
+        # batch_first means that the first dim of the input and output will be the batch_size
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
+        
+        # last, fully-connected layer
+        self.fc = nn.Linear(hidden_dim, output_size)
+
+    def forward(self, x, hidden):
+        # x (batch_size, seq_length, input_size)
+        # hidden (n_layers, batch_size, hidden_dim)
+        # r_out (batch_size, time_step, hidden_size)
+        batch_size = x.size(0)
+        
+        # get RNN outputs
+        r_out, hidden = self.rnn(x, hidden)
+        # shape output to be (batch_size*seq_length, hidden_dim)
+        r_out = r_out.view(-1, self.hidden_dim)  
+        
+        # get final output 
+        output = self.fc(r_out)
+        
+        return output, hidden
+    
+
+
+# test that dimensions are as expected
+test_rnn = RNN(input_size=1, output_size=1, hidden_dim=10, n_layers=2)
+
+# generate evenly spaced, test data pts
+time_steps = np.linspace(0, np.pi, seq_length)
+data = np.sin(time_steps)
+data.resize((seq_length, 1))
+
+test_input = torch.Tensor(data).unsqueeze(0) # give it a batch_size of 1 as first dimension
+print('Input size: ', test_input.size())
+
+# test out rnn sizes
+test_out, test_h = test_rnn(test_input, None)
+print('Output size: ', test_out.size())
+print('Hidden state size: ', test_h.size())
